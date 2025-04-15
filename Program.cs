274d61524a6d1998926ff02d7edaebdc14a0405e
emolesky <emilynnmolesky@gmail.com>
2025-04-15 11:47:48 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ValidParaentheses
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution aSolution = new Solution();
+            Console.WriteLine(aSolution.isValid("()"));
+            Console.WriteLine(aSolution.isValid("{})"));
+            Console.WriteLine(aSolution.isValid("{]"));
+            Console.WriteLine(aSolution.isValid("(){}[]"));
+
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidParaentheses
+{
+    public class Solution
+    {
+        public bool isValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(') stack.Push(')');
+                else if (c == '[') stack.Push(']');
+                else if (c == '{') stack.Push('}');
+                else
+                {
+                    if (stack.Count == 0 || stack.Pop() != c)
+                        return false;
+
+                }
+
+            }
+            return stack.Count == 0;
+        }
+    }
+}
