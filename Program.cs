25a92a0664bcaaa5aa3110fff1a172805577033c
emolesky <emilynnmolesky@gmail.com>
2025-04-15 11:50:27 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-﻿namespace FindPivot
+﻿namespace NumberOfCalls
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Solution aSolution = new Solution();
-            int[] nums1 = { 1, 7, 3, 6, 5, 6 };
-            Console.WriteLine(aSolution.PivotIndex(nums1));
-            int[] nums2 = { 1, 2, 3 };
-            Console.WriteLine(aSolution.PivotIndex(nums2));
-            int[] nums3 = { 2, 1, -1 };
-            Console.WriteLine(aSolution.PivotIndex(nums3));
+            RecentCounter aRecentCounter = new RecentCounter();
+
+            Console.WriteLine(aRecentCounter.Ping(1));
+            Console.WriteLine(aRecentCounter.Ping(100));
+            Console.WriteLine(aRecentCounter.Ping(3001));
+            Console.WriteLine(aRecentCounter.Ping(3002));
+
+
         }
     }
 }
---
+++ RecentCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfCalls
+{
+    public class RecentCounter
+    {
+        private Queue<int> requests;
+        public RecentCounter()
+        {
+            requests = new Queue<int>();
+        }
+
+        public int Ping(int t)
+        {
+            requests.Enqueue(t);
+
+            while ( requests.Peek() < t -3000)
+            {
+                requests.Dequeue();
+            }
+            return requests.Count;
+        }
+    }
+}
